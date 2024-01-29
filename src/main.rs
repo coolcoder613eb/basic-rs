1b8e3b476bc797d5bf25a6432ebe295b9398416e
coolcoder613eb <96163908+coolcoder613eb@users.noreply.github.com>
2024-01-29 14:16:44 +1100

    lex line number and keyword

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ mod tokens;
 use tokens::tokenise;
 fn main() {
     let testcode = "10 PRINT \"Hello, World!\"\n20 GOTO 10";
-    println!("{}", testcode);
+    println!("Program: \n---\n{}\n---", testcode);
     let lines = testcode.lines();
     for line in lines {
         let tokens = tokenise(line);
+        println!("Tokens: {:#?}", tokens)
     }
 }
--- src/tokens.rs
+++ src/tokens.rs
@@ -1,32 +1,55 @@
+#[derive(Debug)]
 pub enum Tokens {
     Linenum,
     Print,
     Goto,
     String,
+    Comment,
+    Unknown,
 }
+#[derive(Debug)]
 pub struct Token {
     tokentype: Tokens,
-    text: str,
+    text: String,
 }
+#[derive(Debug)]
 struct Line {
     index: usize,
     line: String,
 }
-pub fn tokenise(linetext: &str) {
-    println!("{}", linetext);
+pub fn tokenise(linetext: &str) -> Vec<Token> {
+    println!("Line: {}", linetext);
     let mut line = Line {
         index: 0,
         line: String::from(linetext),
     };
-    println!("Line number: {}", getlinenum(&mut line));
-    println!("'{}'", &line.line[line.index..])
+    let linenum = getlinenum(&mut line);
+    let mut tokens = vec![Token {
+        tokentype: Tokens::Linenum,
+        text: String::from(linenum),
+    }];
+    let keyword = readuntil(&mut line, ' ', false);
+    let kwtype = match keyword.to_uppercase().as_str() {
+        "PRINT" => Tokens::Print,
+        "GOTO" => Tokens::Goto,
+        _ => {
+            println!("Unknown keyword!");
+            Tokens::Unknown
+        }
+    };
+    tokens.push(Token {
+        tokentype: kwtype,
+        text: String::from(keyword),
+    });
+    tokens
 }
 fn getlinenum(line: &mut Line) -> &str {
     readuntil(line, ' ', false)
 }
 fn readuntil(line: &mut Line, character: char, esc: bool) -> &str {
-    let index = line.line.find(character).unwrap();
-    let text = &line.line[line.index..index];
+    let left = &line.line[line.index..];
+    let index = left.find(character).unwrap();
+    let text = &line.line[line.index..index + line.index];
     line.index = index + 1;
     text
 }
