91efbadc880cf6d27de3ed0e8a1ce1284ea8693a
coolcoder613eb <96163908+coolcoder613eb@users.noreply.github.com>
2024-02-07 21:57:13 +1100

    Start afresh

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-mod tokens;
-use tokens::tokenise;
 fn main() {
-    let testcode = "10 PRINT \"Hello,\\\"\\n\\\" World!\"\n20 GOTO 10";
-    println!("Program: \n---\n{}\n---", testcode);
-    let lines = testcode.lines();
-    for line in lines {
-        let tokens = tokenise(line);
-        println!("Tokens: {:#?}", tokens)
-    }
+    println!("Hello, world!");
 }
